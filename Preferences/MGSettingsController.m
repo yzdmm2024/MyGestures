58bb2354432639cedff906622b54963815218194
yzdmm2024 <yzdmm2024@users.noreply.github.com>
2026-09-07 08:14:05 +0800

    v0.7.0: 精简为8个验证有效动作(移除抽屉/开关类/媒体/音量等无效项), 保留相机/打开应用

--- Preferences/MGSettingsController.m
+++ Preferences/MGSettingsController.m
@@ -96,7 +96,7 @@ static PSSpecifier *MGSlider(id ctrl, NSString *name, NSString *key)
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    self.title = @"我的手势 0.6.0";
+    self.title = @"我的手势 0.7.0";
     [self attachDiagramHeader];
 }
 
